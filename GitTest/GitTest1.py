6d106c87f334d73891dcb105efbfb55da2be6d61
jaya <jayalakshmy.sp@gmail.com>
2021-03-17 23:04:48 +0000

    This is 2nd commit from Y

--- GitTest/GitTest1.py
+++ GitTest/GitTest1.py
@@ -11,7 +11,7 @@ class Employee:
         self.pay = int(self.pay * self.inc_percent)
 
     def fullname(self):
-        return self.first+' '+self.last
+        return self.first + ' ' + self.last
 
     def __repr__(self):
         return f"Employee('{self.first}','{self.last}','{self.pay}')"
@@ -19,10 +19,9 @@ class Employee:
     def __str__(self):
         return f"{self.fullname()} - {self.email}"
 
-emp_1= Employee('Aarthi','Sivasankar',34323)
 
-h='111'
+emp_1 = Employee('Aarthi', 'Sivasankar', 34323)
+
+h = '111'
 print(f"value of h :{h}")
 print(emp_1)
-
-
